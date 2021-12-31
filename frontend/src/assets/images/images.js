503db89dd91740abaa2a2bd5c67d9161db1b9f9b
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2021-12-31 12:40:57 -0800

    added icons

---
+++ frontend/src/assets/images/images.js
@@ -0,0 +1,39 @@
+// COP Icons
+import CopIconData from "./svgs/cop-icon-datascience.svg";
+import CopIconEngineering from "./svgs/cop-icon-engineering.svg";
+import CopIconOps from "./svgs/cop-icon-ops.svg";
+import CopIconProduct from "./svgs/cop-icon-product.svg";
+import CopIconUiux from "./svgs/cop-icon-uiux.svg";
+
+// Icons
+import IconDownArrow from "./svgs/icon-down-arrow.svg";
+import IconHamburgerMenu from "./svgs/icon-hamburger-menu.svg";
+import IconX from "./svgs/icon-x.svg";
+
+// CTJ Logos
+import LogoHorizontal from "./svgs/logo-horizontal.svg";
+import LogoMark from "./svgs/logo-logomark.svg";
+import LogoStacked from "./svgs/logo-stacked.svg";
+import LogoType from "./svgs/logo-logotype.svg";
+import LogoVertical from "./svgs/logo-vertical.svg";
+import LogoWordmark from "./svgs/logo-wordmark.svg";
+
+export {
+  // COP Icons
+  CopIconData,
+  CopIconEngineering,
+  CopIconOps,
+  CopIconProduct,
+  CopIconUiux,
+  // Icons
+  IconDownArrow,
+  IconHamburgerMenu,
+  IconX,
+  // CTJ Logos
+  LogoHorizontal,
+  LogoMark,
+  LogoStacked,
+  LogoType,
+  LogoVertical,
+  LogoWordmark,
+};
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import * as Images from "../assets/images/images";
 
 function App() {
-    return (
-        <div>
-            <h1>Hi</h1>
-            <div><span className="red">change the browser window size to see the color change responsively,</span> while this does not change at all!</div>
-            <br></br>
-            <div className="red text-center">CivicTechJobs unites ambitious technology practitioners with volunteer opportunities from a central hub of listings to build digital products, programs, and services.</div>
-            <div className="title-1">Title 1</div>
-            <div className="title-2">Title 2</div>
-            <div className="title-3">Title 3</div>
-            <div className="title-4">Title 4</div>
-            <div className="title-5">Title 5</div>
-            <div className="title-6">Title 6</div>
-            <div className="title-7">Title 7</div>
-            <br></br>
-            <div className="paragraph-1">Paragraph 1</div>
-            <div className="paragraph-2">Paragraph 2</div>
-            <div className="paragraph-3">Paragraph 3</div>
-            <div className="paragraph-4">Paragraph 4</div>
-            <div className="paragraph-5">Paragraph 5</div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hi</h1>
+      <div>
+        <span className="red text-center">
+          Change the browser window size to see the color change responsively,
+        </span>{" "}
+        while this does not change at all!
+      </div>
+      <br></br>
+      <img src={Images.LogoHorizontal} width="400"></img>
+      <div>Horizontal</div>
+      <br></br>
+      <img src={Images.LogoMark} width="200"></img>
+      <div>Logomark</div>
+      <br></br>
+      <img src={Images.LogoStacked} width="400"></img>
+      <div>Stacked</div>
+      <br></br>
+      <img src={Images.LogoType} width="400"></img>
+      <div>Logotype</div>
+      <br></br>
+      <img src={Images.LogoVertical} width="400"></img>
+      <div>Vertical</div>
+      <br></br>
+      <img src={Images.LogoWordmark} width="400"></img>
+      <div>Wordmark</div>
+      <br></br>
+      <img src={Images.CopIconData} width="100"></img>
+      <img src={Images.CopIconEngineering} width="100"></img>
+      <img src={Images.CopIconOps} width="100"></img>
+      <img src={Images.CopIconProduct} width="100"></img>
+      <img src={Images.CopIconUiux} width="100"></img>
+      <div>COP Icons</div>
+      <img src={Images.IconDownArrow} width="100"></img>
+      <img src={Images.IconHamburgerMenu} width="100"></img>
+      <img src={Images.IconX} width="100"></img>
+      <div>Majesticons</div>
+      <div className="title-1">Title 1</div>
+      <div className="title-2">Title 2</div>
+      <div className="title-3">Title 3</div>
+      <div className="title-4">Title 4</div>
+      <div className="title-5">Title 5</div>
+      <div className="title-6">Title 6</div>
+      <div className="title-7">Title 7</div>
+      <br></br>
+      <div className="paragraph-1">Paragraph 1</div>
+      <div className="paragraph-2">Paragraph 2</div>
+      <div className="paragraph-3">Paragraph 3</div>
+      <div className="paragraph-4">Paragraph 4</div>
+      <div className="paragraph-5">Paragraph 5</div>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById("app"));
