6431c32dd229bcfbbe5d13068979ad074c22c362
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-01-05 22:21:44 -0800

    configured buttons

--- frontend/src/components/Buttons/Buttons.js
+++ frontend/src/components/Buttons/Buttons.js
@@ -1,24 +1,45 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import React from "react";
 import "./_Buttons.scss";
 
 function Button({ size = "sm", color = "primary", ...props }) {
-  console.log(props)
-
+  const Tag = 'href' in props ? 'a' : 'button'
 
   return (
-    <button className={`btn-${size} btn-${color} ${props.class || ""}`} disabled={true}>
+    <Tag
+      className={combineClasses(
+        `btn-${size}${props.variant ? `-${props.variant}` : ""}`,
+        `btn-${color}`,
+        props.variant,
+        props.class,
+        props.disabled && "disabled"
+      )}
+
+      href={props.href}
+    >
       {props.children}
-    </button>
+    </Tag>
   );
 }
 
-function ButtonLink({ size = "sm", color = "primary", ...props }) {
-  return (
-    <a className={`btn-${size} btn-${color} ${props.class || ""}`}>
-      {props.children}
-    </a>
-  );
+Button.propTypes = {
+  active: PropTypes.bool,
+  class: PropTypes.string,
+  color: PropTypes.oneOf(["primary", "secondary"]),
+  disabled: PropTypes.bool,
+  size: PropTypes.oneOf(["sm", "md", "lg", "icon"]),
+  variant: PropTypes.oneOf(["long"]),
+};
+
+function combineClasses(...args) {
+  let classes = [];
+  for (const arg of args) {
+    if (arg) {
+      classes.push(arg);
+    }
+  }
+
+  return classes.join(" ");
 }
 
-export { Button, ButtonLink };
+export { Button };
