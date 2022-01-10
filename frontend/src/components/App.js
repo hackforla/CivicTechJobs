589625fe983e7b6cc7560e9bbf9f7ac34ed9ffbd
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-01-09 16:46:13 -0800

    updated frontpage demo

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,43 +6,59 @@ import * as Images from "../assets/images/images";
 function App() {
   return (
     <div>
-      <h1>Hi</h1>
-      <ButtonShowcase>
-        {["sm", "md", "lg", "icon"].map((size, index) => {
-          return (
-            <Button key={index} size={size} color="secondary" disabled>
-              Donate
-            </Button>
-          );
-        })}
-      </ButtonShowcase>
-      <ButtonShowcase>
-        {["sm", "md", "lg", "icon"].map((size, index) => {
-          return (
-            <Button href="https://www.google.com/" key={index} size={size} color="secondary">
-              Donate
-            </Button>
-          );
-        })}
-      </ButtonShowcase>
-      <ButtonShowcase>
-        {["sm", "md", "lg"].map((size, index) => {
-          return (
-            <Button key={index} size={size + "-long"} color="primary">
-              Join Us
-            </Button>
-          );
-        })}
-      </ButtonShowcase>
-      <ButtonShowcase>
-        {["sm", "md", "lg"].map((size, index) => {
-          return (
-            <Button key={index} size={size + "-long"} color="primary">
-              Join Us
-            </Button>
-          );
-        })}
-      </ButtonShowcase>
+      <h1>Hello, World!</h1>
+      <h2>Buttons</h2>
+      <Button size="lg" color="primary" class="btn-sm-mobile btn-md-lgtablet">
+        Donate
+      </Button>
+      <Button size="lg" color="secondary" class="btn-sm-mobile btn-md-lgtablet">
+        Donate
+      </Button>
+      <div>
+        <span className="red text-center">
+          These buttons uses button tags and are responsive! Resize your screen to see the breakpoints!
+        </span>
+      </div>
+      <br></br>
+      <br></br>
+      <Button
+        href="https://www.google.com/"
+        size="lg"
+        color="primary"
+        class="btn-sm-mobile btn-md-lgtablet"
+        target="_blank"
+      >
+        Donate
+      </Button>
+      <Button
+        href="https://www.google.com/"
+        size="lg"
+        color="secondary"
+        class="btn-sm-mobile btn-md-lgtablet"
+        target="_blank"
+      >
+        Donate
+      </Button>
+      <div>
+        <span className="red text-center">
+          These buttons uses a tags and are responsive! Click them to open a new tab to Google.
+        </span>
+      </div>
+      <br></br>
+      <br></br>
+      <Button size="icon" color="primary">
+        Donate
+      </Button>
+      <Button size="icon" color="secondary">
+        Donate
+      </Button>
+      <div>
+        <span className="red text-center">
+          These are icon buttons. They are not responsive.
+        </span>
+      </div>
+      <hr></hr>
+      <h2>Logos</h2>
       <img src={Images.LogoHorizontal} height="50"></img>
       <div>Horizontal</div>
       <br></br>
@@ -60,19 +76,20 @@ function App() {
       <br></br>
       <img src={Images.LogoWordmark} height="50"></img>
       <div>Wordmark</div>
-      <br></br>
+      <hr></hr>
+      <h2>COP Icons</h2>
       <img src={Images.CopIconData} height="50"></img>
       <img src={Images.CopIconEngineering} height="50"></img>
       <img src={Images.CopIconOps} height="50"></img>
       <img src={Images.CopIconProduct} height="50"></img>
       <img src={Images.CopIconUiux} height="50"></img>
-      <br></br>
-      <div>COP Icons</div>
+      <hr></hr>
+      <h2>Majesticons</h2>
       <img src={Images.IconDownArrow} height="50"></img>
       <img src={Images.IconHamburgerMenu} height="50"></img>
       <img src={Images.IconX} height="50"></img>
-      <br></br>
-      <div>Majesticons</div>
+      <hr></hr>
+      <h2>Title Classes</h2>
       <div className="title-1">Title 1</div>
       <div className="title-2">Title 2</div>
       <div className="title-3">Title 3</div>
@@ -80,29 +97,14 @@ function App() {
       <div className="title-5">Title 5</div>
       <div className="title-6">Title 6</div>
       <div className="title-7">Title 7</div>
-      <br></br>
+      <hr></hr>
+      <h2>Paragraph Classes</h2>
       <div className="paragraph-1">Paragraph 1</div>
       <div className="paragraph-2">Paragraph 2</div>
       <div className="paragraph-3">Paragraph 3</div>
       <div className="paragraph-4">Paragraph 4</div>
       <div className="paragraph-5">Paragraph 5</div>
     </div>
-  );
-}
-
-function ButtonShowcase(props) {
-  return (
-    <Fragment>
-      {props.children}
-      <div>
-        <span className="red text-center">
-          Change the browser window size to see the color change responsively,
-        </span>{" "}
-        while this does not change at all!
-      </div>
-      <br></br>
-      <br></br>
-    </Fragment>
   );
 }
 
