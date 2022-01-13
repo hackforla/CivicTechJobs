0f97b97c3996db7def2c62a0eca10f86c45646ed
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-01-12 22:39:32 -0800

    started close button

---
+++ frontend/src/components/Buttons/CloseButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import * as utility from "../Utility/utils";
+import { IconX } from "../../assets/images/images";
+import "./_CloseButton.scss";
+
+function CloseButton(props) {
+  return (
+    <img
+      src={IconX}
+      onClick={() => props.onClick}
+      className={utility.combineClasses("btn-close", props.class)}
+    ></img>
+  );
+}
+
+// Type declaration for props
+CloseButton.propTypes = {
+  class: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export { CloseButton };
