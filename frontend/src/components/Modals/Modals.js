150fe6d239c2f712d2f798cb8985f88826d23ce9
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-01-12 22:39:58 -0800

    added closebutton component to modal

--- frontend/src/components/Modals/Modals.js
+++ frontend/src/components/Modals/Modals.js
@@ -1,48 +1,21 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { IconX } from "../../assets/images/images";
+import * as utility from "../Utility/utils";
+import { CloseButton } from "../Buttons/CloseButton";
 import "./_Modals.scss";
 
 function Modal(props) {
-  return <div className="modal">
-      <img src={IconX}></img>
-      {props.children}
-      </div>;
+  return (
+    <div className={utility.combineClasses("modal", props.class)}>
+      <CloseButton class="modal-x"></CloseButton>
+      <div>{props.children}</div>
+    </div>
+  );
 }
 
-/*
 // Type declaration for props
-Button.propTypes = {
+Modal.propTypes = {
   class: PropTypes.string,
-  color: PropTypes.oneOf(["primary", "primary-dark"]),
-  disabled: PropTypes.bool,
-  href: PropTypes.string,
-  onClick: PropTypes.func,
-  size: PropTypes.oneOf([
-    "sm",
-    "md",
-    "lg",
-    "icon",
-    "sm-long",
-    "md-long",
-    "lg-long",
-  ]),
-  target: PropTypes.oneOf([
-    "_blank",
-    "_self",
-    "_parent",
-    "_top",
-    PropTypes.string,
-  ]),
-};*/
-
-/**
- * Takes an array of different and combine them into one string to be placed in an element's class attribute.
- * @param  {...any} args an array of anything
- * @returns a joined string after filtering out non-strings from args
- */
-function combineClasses(...args) {
-  return args.filter((x) => typeof x === "string").join(" ");
-}
+};
 
 export { Modal };
