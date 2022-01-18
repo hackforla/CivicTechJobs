bd77e9816c0a53e97fee411424da9ac3d6ddb352
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-01-18 11:36:06 -0800

    added new webpack configuration for inline svg

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,84 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: "./frontend/src/index.js" // place where the file to render is
+  mode: "development",
+  entry: {
+    index: "./frontend/src/index.js", // place where the file to render is
+  },
+  output: {
+    clean: {
+      keep: ".gitkeep", // cleans all but this named file
     },
-    output: {
-        clean: {
-            keep: '.gitkeep' // cleans all but this named file
+    filename: "[name].[contenthash].js", // forces recaching of css by added hashes to filename
+    path: path.resolve(__dirname, "frontend/static/frontend"), // directory where the bundle is placed
+  },
+  devtool: "inline-source-map", // for debugging, do NOT use in production
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        filename: '[name].[contenthash].js', // forces recaching of css by added hashes to filename
-        path: path.resolve(__dirname, 'frontend/static/frontend'), // directory where the bundle is placed
-    },
-    devtool: 'inline-source-map', // for debugging, do NOT use in production
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                }
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            }, {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ]
-    },
-    optimization: {
-        // all of the following is for chunking to split js into multiple files and prevent reusing code
-        moduleIds: 'deterministic',
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
-        },
-    },
-    plugins: [
-        // automatically adds the hashed js file paths to template
-        new HtmlWebpackPlugin({
-            filename: '../../templates/frontend/index.html', //need to go back because will attempt to create file at output
-            template: '/frontend/src/templates/index.html',
-        }),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        oneOf: [
+          {
+            type: "asset",
+            resourceQuery: /url/, // *.svg?url
+          },
+          {
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
-    watchOptions: {
-        ignored: /node_modules/, // speeds up webpack watch
+  },
+  optimization: {
+    // all of the following is for chunking to split js into multiple files and prevent reusing code
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
     },
-}
+  },
+  plugins: [
+    // automatically adds the hashed js file paths to template
+    new HtmlWebpackPlugin({
+      filename: "../../templates/frontend/index.html", //need to go back because will attempt to create file at output
+      template: "/frontend/src/templates/index.html",
+    }),
+  ],
+  watchOptions: {
+    ignored: /node_modules/, // speeds up webpack watch
+  },
+};
