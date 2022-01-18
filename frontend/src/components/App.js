ecca86e0bca396e7ac4b60158e6325e123abaf38
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-01-17 23:16:37 -0800

    updated demo

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,65 +3,26 @@ import { render } from "react-dom";
 import { CopCard, InnerCopCard } from "./Cards/Cards";
 import { LargeCopCardLayout } from "./Layouts/Layouts";
 import * as Images from "../assets/images/images";
+import { InnerCopNav } from "./Navigation/_Navigation";
 
 function App() {
   return (
     <div style={{ margin: "10px" }}>
       <h1>Hello, World!</h1>
-      <h2>Playground</h2>
-      <div class="flex-container">
-        <div class="flex-item col-6">
-          <div className="spaces">1</div>
-        </div>
-        <div class="flex-item col-1">
-          <div className="spaces2">2</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>3</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>4</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>5</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>6</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>7</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>8</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>9</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>10</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>11</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>12</div>
-        </div>
-        <div class="flex-item col-1">
-          <div>13</div>
-        </div>
-      </div>
-      <hr></hr>
       <h2>Cards</h2>
       <div style={{ backgroundColor: "rgba(0,0,0,0.4)", padding: "20px 30px" }}>
         <CopCard hidden={false} size="lg">
-          <InnerCopCard>
-            <div className="title-3">COP Card - Not Mobile</div>
-            <span className="red">
-              Shrink your viewport to see this transition into the mobile
-              version. Also, click the "x" button to see the card disappear.{" "}
-            </span>
-            {lorem}
-          </InnerCopCard>
+          <LargeCopCardLayout>
+            <InnerCopNav></InnerCopNav>
+            <InnerCopCard>
+              <div className="title-3">COP Card - Not Mobile</div>
+              <span className="red">
+                Shrink your viewport to see this transition into the mobile
+                version. Also, click the "x" button to see the card disappear.{" "}
+              </span>
+              {lorem}
+            </InnerCopCard>
+          </LargeCopCardLayout>
         </CopCard>
         <CopCard hidden={false} size="sm">
           <div className="title-4">COP Card - Mobile</div>
