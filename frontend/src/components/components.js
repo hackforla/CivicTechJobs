7399f68172dae676f85e6de6c017b1672cdd6c32
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-02-09 12:36:47 -0800

    made a js file to route components

---
+++ frontend/src/components/components.js
@@ -0,0 +1,17 @@
+import { Button } from "./Buttons/Buttons";
+import { CloseButton } from "./Buttons/CloseButton";
+import { CopCard, InnerCopCard, InnerCopNavCard } from "./Cards/Cards";
+import { ScrollCarousel } from "./Carousel/ScrollCarousel";
+import { InnerCopNav } from "./Navigation/InnerCopNav";
+import { HeaderNav } from "./Navigation/HeaderNav";
+
+export {
+  Button,
+  CloseButton,
+  CopCard,
+  InnerCopCard,
+  InnerCopNavCard,
+  ScrollCarousel,
+  InnerCopNav,
+  HeaderNav,
+};
