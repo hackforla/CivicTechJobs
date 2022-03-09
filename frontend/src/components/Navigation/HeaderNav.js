c9cd422e80547cce43bfaae8b15d7ac4aed532dd
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-03-09 10:22:44 -0800

    cleaned header bug

--- frontend/src/components/Navigation/HeaderNav.js
+++ frontend/src/components/Navigation/HeaderNav.js
@@ -1,13 +1,13 @@
 // External Imports
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
 // Internal Imports
 import * as utility from "../Utility/utils";
 import { IconHamburgerMenu } from "../../assets/images/images";
 import { Button } from "../Buttons/Buttons";
 
-function HeaderNav({ logoDesktop, logoMobile, menu, ...props }) {
+function HeaderNav({ logoDesktop, logoMobile, menu }) {
   const [open, setOpen] = useState(false);
 
   const Logo = () => {
@@ -22,42 +22,50 @@ function HeaderNav({ logoDesktop, logoMobile, menu, ...props }) {
   };
 
   return (
-    <nav className="header-nav flex-container align-center py-1 px-3">
-      <div className="row header-nav-logo">
-        <Logo />
-      </div>
-      <div className="row header-nav-menu justify-right align-center">
-        <div
-          className={utility.combineClasses(
-            "hamburger-menu"
-            //!open && "hidden"
-          )}
-        >
-          {menu.map((item, index) => {
-            return (
-              <a className="header-nav-link mr-4" href={item.link} key={index}>
-                {item.name}
-              </a>
-            );
-          })}
+    <Fragment>
+      <nav className="header-nav flex-container align-center py-1 px-3">
+        <div className="row header-nav-logo">
+          <Logo />
         </div>
+        <div className="row header-nav-menu justify-right align-center">
+          <div
+            className={utility.combineClasses(
+              "hamburger-menu"
+              //!open && "hidden"
+            )}
+          >
+            {menu.map((item, index) => {
+              return (
+                <a
+                  className="header-nav-link mr-4"
+                  href={item.link}
+                  key={index}
+                >
+                  {item.name}
+                </a>
+              );
+            })}
+          </div>
 
-        <Button href="https://www.google.com/" color="primary" size="sm">
-          Log In
-        </Button>
-        <button
-          className="hamburger-icon ml-3"
-          aria-expanded="false"
-          aria-controls="menu"
-          onClick={() => {
-            setOpen(false);
-            alert("menu is now open");
-          }}
-        >
-          <IconHamburgerMenu />
-        </button>
-      </div>
-    </nav>
+          <Button href="https://www.google.com/" color="primary" size="sm">
+            Log In
+          </Button>
+          <button
+            className="hamburger-icon ml-3"
+            aria-expanded="false"
+            aria-controls="menu"
+            onClick={() => {
+              setOpen(false);
+              alert("menu is now open");
+            }}
+          >
+            <IconHamburgerMenu />
+          </button>
+        </div>
+      </nav>
+
+      <div aria-hidden="true"></div>
+    </Fragment>
   );
 }
 
