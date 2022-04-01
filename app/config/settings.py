7e7e38d665aa4bd03ea4d75f97c81f142fc6471d
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-03-31 23:08:47 -0700

    took out default password from settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
         "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
