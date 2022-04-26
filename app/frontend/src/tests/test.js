b40ed4b921d125a474db3c812ff3016b0fc6765a
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-04-26 09:43:25 -0700

    added preliminary tests and gha

--- app/frontend/src/tests/test.js
+++ app/frontend/src/tests/test.js
@@ -1,12 +1,53 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { getRoles, render, screen } from "@testing-library/react";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 
 import { Button } from "components/components";
-
+/*
+getByText
+getByRole
+getByLabelText
+getByPlaceholderText
+getByAltText
+getByDisplayValue
+*/
+/*
+queryByText
+queryByRole
+queryByLabelText
+queryByPlaceholderText
+queryByAltText
+queryByDisplayValue
+*/
+/*
+findByText
+findByRole
+findByLabelText
+findByPlaceholderText
+findByAltText
+findByDisplayValue
+*/
+/*
+getAllBy
+queryAllBy
+findAllBy
+*/
 describe("Button", () => {
-  test("renders Button component", () => {
-    render(<Button />);
+  test("Button component", () => {
+    render(<Button>Log in</Button>);
+    expect(screen.getByText("Log in")).toBeInTheDocument();
+    expect(screen.queryByText("Log out")).not.toBeInTheDocument();
+  });
 
-    screen.debug();
+  test("Button tag accessibility", () => {
+    render(<Button />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.queryByRole("link")).not.toBeInTheDocument();
+  });
+
+  test("Button tag accessibility with link", () => {
+    render(<Button href="www.google.com" />);
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.queryByRole("button")).not.toBeInTheDocument();
   });
 });
