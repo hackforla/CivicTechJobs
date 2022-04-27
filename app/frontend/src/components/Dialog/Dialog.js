e8dd7c55a9e11ee87c83181b200d0893bfa79fab
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-04-27 10:15:41 -0700

    added new library to better support transitions

--- app/frontend/src/components/Dialog/Dialog.js
+++ app/frontend/src/components/Dialog/Dialog.js
@@ -1,13 +1,13 @@
 // External Imports
 import PropTypes from "prop-types";
 import React, { Fragment, useState, useEffect, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 
 // Internal Imports
 import { combineClasses } from "../Utility/utils";
 
 function Dialog({ open = false, ...props }) {
   const [isOpen, setIsOpen] = useState(open);
-  const [isClosing, setIsClosing] = useState(false);
 
   const windowRef = useRef(null);
 
@@ -23,27 +23,17 @@ function Dialog({ open = false, ...props }) {
     } else {
       document.body.style.removeProperty("padding-right");
       document.body.style.overflow = "auto";
-      props.onClose();
     }
   }, [isOpen]);
 
   // The next two useEffect and handleClose function ensures that rather than closing immediately, an animation is run when dialog is set to be closed
   useEffect(() => {
-    open ? setIsOpen(open) : setIsClosing(true);
+    if (open) setIsOpen(open);
   }, [open]);
-
-  useEffect(() => {
-    if (isClosing) {
-      setTimeout(() => {
-        setIsOpen(false);
-        setIsClosing(false);
-      }, 400);
-    }
-  }, [isClosing]);
 
   function handleClose(e) {
     if (e.target === windowRef.current) {
-      setIsClosing(true);
+      props.onClose();
     }
   }
 
@@ -53,14 +43,21 @@ function Dialog({ open = false, ...props }) {
         className={combineClasses(
           "dialog",
           !isOpen && "hidden",
-          isClosing && "dialog-close",
           props.addClass
         )}
         ref={windowRef}
         onClick={(e) => handleClose(e)}
         role="dialog"
       >
-        {props.children}
+        <CSSTransition
+          in={open}
+          classNames="dialog"
+          timeout={400}
+          unmountOnExit
+          onExited={() => setIsOpen(false)}
+        >
+          {props.children}
+        </CSSTransition>
       </div>
     </Fragment>
   );
