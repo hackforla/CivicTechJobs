b0290b0954f8e6d18dfcb330943bafddb2be422e
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-04-27 10:51:59 -0700

    moved file mocks

--- app/jest.config.js
+++ app/jest.config.js
@@ -5,10 +5,10 @@ module.exports = async () => {
     moduleFileExtensions: ["js", "jsx"],
     moduleDirectories: ["node_modules", "frontend/src"],
     moduleNameMapper: {
-      "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+      "\\.(css|less)$": "<rootDir>/tests/__mocks__/styleMock.js",
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-        "<rootDir>/__mocks__/fileMock.js",
-      "\\.(svg)(\\?url)$": "<rootDir>/__mocks__/fileMock.js",
+        "<rootDir>/tests/__mocks__/fileMock.js",
+      "\\.(svg)(\\?url)$": "<rootDir>/tests/__mocks__/fileMock.js",
     },
     testEnvironment: "jsdom",
   };
