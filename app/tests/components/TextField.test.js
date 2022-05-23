8a00b6a62926e1a4ce4949a4ed2c87bac8043e34
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-05-23 10:48:50 -0700

    updated tests

--- app/tests/components/TextField.test.js
+++ app/tests/components/TextField.test.js
@@ -14,14 +14,18 @@ config.disabled = true;
 describe("TextField", () => {
   test("TextField component", async () => {
     const user = userEvent.setup();
-    render(<TextField label="Full Stack Developer" />);
-    expect(screen.getByText("Full Stack Developer")).toBeInTheDocument();
-    expect(screen.queryByText("Front end Developer")).not.toBeInTheDocument();
+    render(<TextField label="Email" />);
+    expect(screen.getByLabelText("Email")).toBeInTheDocument();
+    expect(screen.queryByLabelText("Password")).not.toBeInTheDocument();
 
-    await user.click(screen.getByLabelText(/Full Stack Developer/));
-    expect(await screen.queryByRole("checkbox")).toBeChecked();
+    await user.click(screen.getByRole("textbox"));
+    await user.keyboard("foo");
+    expect(await screen.getByDisplayValue("foo")).toBeInTheDocument();
 
-    await user.click(screen.getByLabelText(/Full Stack Developer/));
-    expect(await screen.queryByRole("checkbox")).not.toBeChecked();
+    await user.keyboard("{tab}{tab}");
+    expect(await screen.getByRole("textbox")).toHaveFocus();
+
+    await user.keyboard("bar");
+    expect(await screen.getByDisplayValue("bar")).toBeInTheDocument();
   });
 });
