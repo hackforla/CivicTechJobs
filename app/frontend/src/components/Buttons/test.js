bec05b954355a6fe7d74fa94ae95121ed9a7a9b0
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-07-07 20:13:36 -0700

    tst

---
+++ app/frontend/src/components/Buttons/test.js
@@ -0,0 +1,15 @@
+var pattern1 = /\x00/;
+var pattern2 = /\x0C/;
+const n = 4;
+let a;
+if (n === 1) {
+  a = 1;
+} else if (n === 2) {
+  a = 1;
+} else if (n === 3) {
+  a = 1;
+} else if (n === 2) {
+  a = 1;
+} else if (n === 5) {
+  a = 1;
+}
--- app/frontend/src/pages/LandingPage/LandingPage.tsx
+++ app/frontend/src/pages/LandingPage/LandingPage.tsx
@@ -11,9 +11,21 @@ import {
 import { HeaderNav, FooterNav } from "components/components";
 import { LandingPageIntro } from "./LandingPageIntro";
 import { LandingPageCop } from "./LandingPageCop";
-
 var pattern1 = /\x00/;
 var pattern2 = /\x0C/;
+const n = 4;
+let a;
+if (n === 1) {
+  a = 1;
+} else if (n === 2) {
+  a = 1;
+} else if (n === 3) {
+  a = 1;
+} else if (n === 2) {
+  a = 1;
+} else if (n === 5) {
+  a = 1;
+}
 
 function LandingPage() {
   return (
