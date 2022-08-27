204761ed58995778462e299d5e0aa345a88e409e
Aveline-art <62368440+Aveline-art@users.noreply.github.com>
2022-08-27 12:39:36 -0700

    chip draft

--- app/frontend/src/components/Inputs/Chip.tsx
+++ app/frontend/src/components/Inputs/Chip.tsx
@@ -1,13 +1,35 @@
 // Eternal Imports
+import { IconX, iconX } from "assets/images/images";
 import React from "react";
 
 // Internal Imports
 import { combineClasses } from "../Utility/utils";
 
-interface ChipProps {}
+interface ChipProps {
+  active: boolean;
+  children: React.ReactNode;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
 
-function Chip({ ...props }: ChipProps) {
-  return <div></div>;
+function Chip({ active, children, onClick }: ChipProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={combineClasses("chip", active && "active")}
+    >
+      {active && (
+        <IconX
+          width="12"
+          height="8"
+          fill="#fff"
+          stroke="#fff"
+          viewBox="0 0 12 8"
+          aria-hidden="true"
+        />
+      )}
+      {children}
+    </button>
+  );
 }
 
 export { Chip };
