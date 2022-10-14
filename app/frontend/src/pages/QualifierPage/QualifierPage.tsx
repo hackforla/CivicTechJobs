fc035dcb4fceb3cde3085223dae642e0fc0e7602
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2022-10-14 10:04:38 -0400

    expanded lazy loading

--- app/frontend/src/pages/QualifierPage/QualifierPage.tsx
+++ app/frontend/src/pages/QualifierPage/QualifierPage.tsx
@@ -18,30 +18,30 @@ function QualifierPage(): JSX.Element {
 
   return (
     <Fragment>
-      <HeaderNav
-        logoDesktop={logoHorizontal}
-        logoMobile={logoStacked}
-        menu={[
-          { name: "Hack for LA", link: "/" },
-          { name: "How to Join", link: "/qualifier" },
-          { name: "Projects", link: "/demo" },
-        ]}
-      />
-      <main>
-        <Suspense fallback={<div>...Loading</div>}>
+      <Suspense fallback={<div>...Loading</div>}>
+        <HeaderNav
+          logoDesktop={logoHorizontal}
+          logoMobile={logoStacked}
+          menu={[
+            { name: "Hack for LA", link: "/" },
+            { name: "How to Join", link: "/qualifier" },
+            { name: "Projects", link: "/demo" },
+          ]}
+        />
+        <main>
           <ProgressBar label="Page 1" addClass="px-5" />
           <QualifierPageCalendar setPage={setPage} />
-        </Suspense>
-      </main>
-      <FooterNav
-        logoDesktop={logoHorizontalOnDark}
-        logoMobile={logoStackedOnDark}
-        menu={[
-          { name: "Credits", link: "/" },
-          { name: "Sitemap", link: "/" },
-          { name: "Join Us", link: "/" },
-        ]}
-      />
+        </main>
+        <FooterNav
+          logoDesktop={logoHorizontalOnDark}
+          logoMobile={logoStackedOnDark}
+          menu={[
+            { name: "Credits", link: "/" },
+            { name: "Sitemap", link: "/" },
+            { name: "Join Us", link: "/" },
+          ]}
+        />
+      </Suspense>
     </Fragment>
   );
 }
