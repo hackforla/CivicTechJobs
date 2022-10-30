dbe4fe5cda46060b1dd7110392b9ba4f0a8de394
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2022-10-30 07:36:49 -0400

    delete unused variable

--- app/frontend/src/components/Utility/hooks/dragToSelectUnselect.ts
+++ app/frontend/src/components/Utility/hooks/dragToSelectUnselect.ts
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function dissect(str: string, partition: number = 7) {
   const arr = str.match(new RegExp(`.{1,${partition}}`, "g"));
