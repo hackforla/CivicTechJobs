45ba1d9696be0787be16c658507003e9fe61ae15
Kevin Park <kevin.pmw.park@gmail.com>
2022-11-23 03:44:04 +0000

    remove workflow to deploy to prod, update entrypoint to get ECS healthcheck IP, update ALLOWED_HOSTS with IP and update env vars for DB

--- app/config/settings.py
+++ app/config/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") != "False"
 
-# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
+if "ECS_ALLOWED_HOST" in os.environ:
+    ALLOWED_HOSTS.append(os.environ.get("ECS_ALLOWED_HOST"))
 # Application definition
 
 INSTALLED_APPS = [
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [[ $TARGET == "ECS" ]]; then
+    export ECS_ALLOWED_HOST=$(curl -s ${ECS_CONTAINER_METADATA_URI} | jq -r .Networks[0].IPv4Addresses[0])
+fi
+
 python manage.py collectstatic && \
     python manage.py makemigrations && \
     python manage.py migrate && \
