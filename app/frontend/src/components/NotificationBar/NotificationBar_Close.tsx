4b3e56ba3e243a6f28e9c58b0e90b964aa6f6bb8
Aveline-art <62368440+Aveline-art@users.noreply.github.com>
2023-02-17 11:02:29 -0800

    added transitions

--- app/frontend/src/components/NotificationBar/NotificationBar_Close.tsx
+++ app/frontend/src/components/NotificationBar/NotificationBar_Close.tsx
@@ -1,34 +1,44 @@
 // Eternal Imports
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
 
 // Internal Imports
 import { iconX } from "../../assets/images/images";
-import { combineClasses } from "../Utility/utils";
-import "./_transition.scss";
-import "./_closeButton.scss";
-import "../Basics/_alignment.scss";
 // Type declaration for props
-interface NotificationBarProps {
-  content: string;
+interface NotificationBarProps extends React.PropsWithChildren {
+  autoHidden: boolean; // 1. Added new prop to indicate the bar disappearing on its own
 }
 
-function NotificationBar_Close({ content }: NotificationBarProps) {
+function NotificationBar_Close({
+  autoHidden = false,
+  ...props
+}: NotificationBarProps) {
   const [showBar, setShowBar] = useState(true);
+  const nodeRef = useRef(null);
+
+  // 2. Added CSS Transition component to wrap around notification bar
   return (
-    <>
-      {showBar ? (
-        <div className={combineClasses("bar", "slide-in")}>
-          <button
-            onClick={() => setShowBar(false)}
-            onKeyDown={() => null}
-            className="close"
-          >
-            <img src={iconX} height={"10px"} width={"10px"} alt="close" />
-          </button>
-          <p className={combineClasses("text-center")}>{content}</p>
-        </div>
-      ) : null}
-    </>
+    <CSSTransition
+      in={showBar}
+      nodeRef={nodeRef}
+      classNames="notification-bar-fade"
+      timeout={300}
+      appear
+      unmountOnExit
+      onEntered={() => {
+        if (autoHidden) setShowBar(false);
+      }}
+    >
+      <div ref={nodeRef} className="notification-bar">
+        <button
+          onClick={() => setShowBar(false)}
+          className="notification-bar-close"
+        >
+          <img src={iconX} height={"10px"} width={"10px"} alt="close" />
+        </button>
+        <p className="text-center">{props.children}</p>
+      </div>
+    </CSSTransition>
   );
 }
 
