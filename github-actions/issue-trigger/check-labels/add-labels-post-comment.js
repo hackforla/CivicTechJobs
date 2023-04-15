15501c502476bca2122622c5a43e569719b0118f
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-04-14 21:49:06 -0700

    Fixed bug

--- github-actions/issue-trigger/check-labels/add-labels-post-comment.js
+++ github-actions/issue-trigger/check-labels/add-labels-post-comment.js
@@ -21,7 +21,7 @@ async function main({ g, c }) {
   console.log(`owner: ${context.repo.owner}`)
 
 
-  const addLabelsAction = await github.issues.addLabels({
+  const addLabelsAction = await github.rest.issues.addLabels({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
@@ -31,7 +31,7 @@ async function main({ g, c }) {
   const creator = await context.payload.sender.login;
   console.log(`creator: ${creator}`);
 
-  const postCommentAction = await github.issues.createComment({
+  const postCommentAction = await github.rest.issues.createComment({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
