c4b3f154ef6f102037dc24d9127db9abdcb970fc
Aveline-art <62368440+Aveline-art@users.noreply.github.com>
2023-04-21 21:33:49 -0700

    added transition wrapper to aid in creating transitions

---
+++ app/frontend/src/components/Transition/Wrapper.tsx
@@ -0,0 +1,50 @@
+// External exports
+import React, { useState, useRef, Fragment, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
+
+interface TransitionWrapper extends React.PropsWithChildren {
+  autoExit?: boolean;
+  onExited?: () => any;
+  show?: boolean;
+}
+
+const TransitionWrapper = ({
+  autoExit = false,
+  show = true,
+  ...props
+}: TransitionWrapper) => {
+  const [isNotificationOnDOM, setIsNotificationOnDOM] = useState(show);
+  const nodeRef = useRef(null);
+  const autoHiddenTimeout = 500;
+
+  useEffect(() => {
+    setIsNotificationOnDOM(show);
+  }, [show]);
+
+  return (
+    <CSSTransition
+      in={isNotificationOnDOM}
+      nodeRef={nodeRef}
+      classNames="fade"
+      timeout={400}
+      unmountOnExit
+      appear
+      onEntered={() => {
+        if (autoExit) {
+          window.setTimeout(() => {
+            setIsNotificationOnDOM(false);
+          }, autoHiddenTimeout);
+        }
+      }}
+      onExited={() => {
+        if (props.onExited) {
+          props.onExited();
+        }
+      }}
+    >
+      <div ref={nodeRef}>{props.children}</div>
+    </CSSTransition>
+  );
+};
+
+export { TransitionWrapper };
