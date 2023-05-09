9136e29bef5539e7bf666a685f3ad27269b4e0f0
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-05-08 17:21:41 -0700

    fix command execution in script and add documentation notes

--- alias.sh
+++ alias.sh
@@ -3,12 +3,6 @@
 # Configuration file containing shortened versions and associated commands
 config_file='alias-config.txt'
 
-# Check if the config file exists
-if [ ! -f "$config_file" ]; then
-  echo "Configuration file not found: $config_file"
-  exit 1
-fi
-
 # Function to display help message
 show_help() {
   echo "Usage: $0 <alias>"
@@ -31,9 +25,7 @@ fi
 # Read the config file to find the matching shortened version
 while IFS=':' read -r alias command; do
   if [ "$alias" = "$1" ]; then
-    # The -q flag suppresses the output from the script command itself
-    # The -c flag specifies the command to run and /dev/null discards the typescript file that script would normally generate
-    script -q -c "$command" /dev/null
+    $command
     exit 0
   fi
 done < "$config_file"
