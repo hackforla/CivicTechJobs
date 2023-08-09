31a520c6a008e5a3775193a97e26814f75420aeb
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-08-09 14:37:24 -0700

    Imported tailwindcss/tailwind.css instead of creating separate css file for directives

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "tailwindcss/tailwind.css";
 import "./index.scss";
 
 if (process.env.MODE !== "production") {
