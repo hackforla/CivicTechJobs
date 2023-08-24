f23c425b8acd14239b7ac0a64cfdd04f3a07b3e8
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-08-24 10:35:01 -0700

    Ran prettier

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./src/pages/Demo/DemoTailwind.tsx"], // Will change to "./src/**/*.{js,jsx,tsx}", "./templates/index.html"
   theme: {
     screens: {
-      'sm': '577px',
+      sm: "577px",
       // => @media (min-width: 577px) { ... }
-      'md': '769px',
+      md: "769px",
       // => @media (min-width: 769px) { ... }
-      'lg': '1025px',
+      lg: "1025px",
       // => @media (min-width: 1025px) { ... }
-      'xl': '1201px',
+      xl: "1201px",
       // => @media (min-width: 1201px) { ... }
     },
     colors: {
@@ -19,38 +19,38 @@ module.exports = {
       "blue-dark-hover": "#445ea9",
       "blue-dark-focused": "#273c79",
       // Primary on Dark Colors
-      'blue': "#44aff1",
+      blue: "#44aff1",
       "blue-focused": "#3fa1de",
       // Secondary Colors
-      'tan': "#ffe0b9",
+      tan: "#ffe0b9",
       "tan-light": "#ffefdb",
-      'green': "#13831e",
+      green: "#13831e",
       // Neutral Colors
-      'white': "#fff",
+      white: "#fff",
       "grey-light": "#f2f2f2",
-      'grey': "#c1c1c1",
+      grey: "#c1c1c1",
       "grey-dark": "#585858",
-      'charcoal': "#333",
+      charcoal: "#333",
     },
     fontFamily: {
-      'sans': ['Roboto', 'Tahoma', 'Verdana', 'sans-serif'],
+      sans: ["Roboto", "Tahoma", "Verdana", "sans-serif"],
     },
     fontWeight: {
-      thin: '100',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      bold: '700',
-      black: '900',
+      thin: "100",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      bold: "700",
+      black: "900",
     },
     extend: {
       lineHeight: {
-        'extra-tight': '1.125rem',
+        "extra-tight": "1.125rem",
       },
       borderRadius: {
-        DEFAULT: '20px',
-        'large': '60px',
-        'x-large': '100px',
+        DEFAULT: "20px",
+        large: "60px",
+        "x-large": "100px",
       },
     },
   },
