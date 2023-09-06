3a2603c1be744dbba66f2f20fb0b46741a3eb63f
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-09-06 14:40:00 -0700

    Deleted duplicated route

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -54,10 +54,6 @@ const router = createBrowserRouter([
             element: <DemoTailwind />,
           },
           {
-            path: "demo",
-            element: <Demo />,
-          },
-          {
             path: "*",
             element: <NotFoundPage />,
           },
