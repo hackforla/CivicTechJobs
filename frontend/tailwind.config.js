f6d789a92ce43df855db3089f6982cffc89cead6
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-10-30 15:41:15 -0700

    Formatted tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -59,9 +59,9 @@ module.exports = {
         "x-large": "100px",
       },
       rotate: {
-        '290': '290deg',
-        '345': '345deg',
-      }
+        290: "290deg",
+        345: "345deg",
+      },
     },
   },
   plugins: [],
