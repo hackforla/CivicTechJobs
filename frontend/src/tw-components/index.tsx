45ab9b6498f20f10cf3ede1b192dee5768455091
bennyv8 <bvanhr@gmail.com>
2023-10-31 17:11:40 -0700

    updated tw-components index.tsx to include cookiebanner

--- frontend/src/tw-components/index.tsx
+++ frontend/src/tw-components/index.tsx
@@ -1,3 +1,4 @@
 export { default as AuthNav } from "./AuthNav";
 export { default as HeaderNav } from "./HeaderNav";
 export { default as InputGroup } from "./InputGroup";
+export { default as CookieBanner } from "./CookieBanner";
