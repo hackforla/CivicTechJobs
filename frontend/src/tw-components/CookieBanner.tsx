3338e72aa44fe072702761db82e4b1a78d122ec7
bennyv8 <bvanhr@gmail.com>
2023-11-01 17:28:07 -0700

    should highlight and hover clickable text
    "no thanks" should be able to highlight and hover
    "Cookie Policy" should be able to highlight and hover

--- frontend/src/tw-components/CookieBanner.tsx
+++ frontend/src/tw-components/CookieBanner.tsx
@@ -33,7 +33,7 @@ function CookieBanner(props: CookieBannerProps) {
         <p className="max-w-[80%]">
           We use cookies to improve your experience. By clicking "Accept
           Cookies", you are agreeing to the collection of data as described in
-          our <a >Cookie Policy</a>
+          our <a className="text-blue-dark-hover underline cursor-pointer hover:text-blue-dark-focused">Cookie Policy</a>
         </p>
         <div className="flex flex-row items-center space-x-6">
           <Button color="primary" size="md">
@@ -43,7 +43,7 @@ function CookieBanner(props: CookieBannerProps) {
             onClick={(e) => {
               setIsHidden(true);
             }}
-            className="hover:underline hover:font-bold"
+            className="text-blue-dark-hover cursor-pointer hover:text-blue-dark-focused pl-2"
           >
             No Thanks
           </a>
