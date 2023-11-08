861a296036c9c784435d8f368705681b0fbf9f6b
bennyv8 <bvanhr@gmail.com>
2023-11-07 16:49:32 -0800

    removed changed export for input group

--- frontend/src/tw-components/index.tsx
+++ frontend/src/tw-components/index.tsx
@@ -1,5 +1,4 @@
 export { default as AuthNav } from "./AuthNav";
 export { default as HeaderNav } from "./HeaderNav";
-export { default as InputGroup } from "./InputGroup";
 export { default as CookieBanner } from "./CookieBanner";
 export { default as TextField } from "./TextField";
