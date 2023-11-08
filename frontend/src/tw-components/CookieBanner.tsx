12917747e2874c891fa10672015ddb6c372fdad5
bennyv8 <bvanhr@gmail.com>
2023-11-07 23:09:07 -0800

    Tidied styling inconsistencies
    Added more padding for content within banner
    Add more space between the buttons
    Fixed on-hover for buttons w/ shaddow & correct color matching
    Added inner-shaddow for banner to make it pop more

--- frontend/src/tw-components/CookieBanner.tsx
+++ frontend/src/tw-components/CookieBanner.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Cookies from 'js-cookie';
 
 // Internal Imports
-import { Button } from "../components/components";
 import { IconButton } from "components/components";
 import { iconX } from "assets/images/images";
 
@@ -28,9 +27,9 @@ function CookieBanner(props: CookieBannerProps) {
       aria-label="cookies banner"
       className={`${
         hidden ? "hidden" : ""
-      } fixed flex flex-col bottom-12 left-1/2 transform -translate-x-1/2 bg-white w-3/4 p-4 z-50W rounded-lg shadow-xl`}
+      } fixed flex flex-col bottom-12 left-1/2 transform -translate-x-1/2 bg-white w-3/4 p-4 z-50W rounded-lg shadow-2xl shadow-inner`}
     >
-      <div className="min-h-48 max-h-64 justify-between space-y-5">
+      <div className="min-h-48 max-h-64 justify-between space-y-5 p-6">
         <div className="flex flex-row justify-between items-center">
           <p className="text-xl font-bold">This site use cookies!</p>
           <IconButton
@@ -42,15 +41,15 @@ function CookieBanner(props: CookieBannerProps) {
         <p className="max-w-[80%]">
           We use cookies to improve your experience. By clicking "Accept
           Cookies", you are agreeing to the collection of data as described in
-          our <a href='/' className="text-blue-dark-hover underline cursor-pointer hover:text-blue-dark-focused">Cookie Policy</a>
+          our <a href='/' className="text-blue-dark underline cursor-pointer hover:text-blue-dark-hover focus:bg-blue-dark-focused">Cookie Policy</a>
         </p>
-        <div className="flex flex-row items-center space-x-6">
-          <Button color="primary" size="md" onClick={handleAcceptCookies}>
+        <div className="flex flex-row items-center space-x-12">
+          <button className="rounded px-10 py-1 bg-blue-dark text-white hover:bg-blue-dark-hover hover:shadow-lg focus:bg-blue-dark-focused" onClick={handleAcceptCookies}>
             Accept
-          </Button>
+          </button>
           <button
             onClick={handleDeclineCookies}
-            className="text-blue-dark-hover cursor-pointer hover:text-blue-dark-focused pl-2"
+            className="text-blue-dark cursor-pointer hover:text-blue-dark-hover focus:bg-blue-dark-focused hover:drop-shadow-lg"
           >
             No Thanks
           </button>
