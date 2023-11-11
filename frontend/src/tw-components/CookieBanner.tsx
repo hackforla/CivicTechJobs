7c1eea71abea8ed8dc1fe44bd1b6a74e0f13c35e
bennyv8 <bvanhr@gmail.com>
2023-11-10 22:19:33 -0800

    Updated Cookie Banner with correct routing
    added privacy policy page href tag
    changed header to correct text-color

--- frontend/src/tw-components/CookieBanner.tsx
+++ frontend/src/tw-components/CookieBanner.tsx
@@ -27,11 +27,11 @@ function CookieBanner(props: CookieBannerProps) {
       aria-label="cookies banner"
       className={`${
         hidden ? "hidden" : ""
-      } fixed flex flex-col bottom-12 left-1/2 transform -translate-x-1/2 bg-white w-3/4 p-4 z-50W rounded-lg shadow-2xl shadow-inner`}
+      } fixed flex flex-col bottom-12 left-1/2 transform -translate-x-1/2 bg-white w-3/4 p-4 z-50 rounded-lg shadow-2xl shadow-inner`}
     >
       <div className="min-h-48 max-h-64 justify-between space-y-5 p-6">
         <div className="flex flex-row justify-between items-center">
-          <p className="text-xl font-bold">This site use cookies!</p>
+          <p className="text-xl font-bold text-charcoal">This site use cookies!</p>
           <IconButton
             iconUrl={iconX}
             label="close"
@@ -41,7 +41,7 @@ function CookieBanner(props: CookieBannerProps) {
         <p className="max-w-[80%]">
           We use cookies to improve your experience. By clicking "Accept
           Cookies", you are agreeing to the collection of data as described in
-          our <a href='/' className="text-blue-dark underline cursor-pointer hover:text-blue-dark-hover focus:bg-blue-dark-focused">Cookie Policy</a>
+          our <a href='/privacypolicy' className="text-blue-dark underline cursor-pointer hover:text-blue-dark-hover focus:bg-blue-dark-focused">Cookie Policy</a>
         </p>
         <div className="flex flex-row items-center space-x-12">
           <button className="rounded px-10 py-1 bg-blue-dark text-white hover:bg-blue-dark-hover hover:shadow-lg focus:bg-blue-dark-focused" onClick={handleAcceptCookies}>
