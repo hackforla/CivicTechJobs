fec427994a70524211f84c44387e00ba57736f8c
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2024-02-05 10:35:48 -0800

    Refactored LandingPageIntro with Tailwind

--- frontend/src/pages/LandingPage/LandingPageIntro.tsx
+++ frontend/src/pages/LandingPage/LandingPageIntro.tsx
@@ -1,37 +1,52 @@
 // External Imports
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Internal Imports
-import { iconArrowDown } from "assets/images/images";
-import { Button } from "components/components";
+import {
+  iconArrowDown,
+  LandingPageBg,
+  LandingPageFg,
+} from "assets/images/images";
 
 function LandingPageIntro() {
   return (
-    <div className="landing-container my-10">
-      <div className="flex-column align-center text-center landing-intro-container">
-        <h1 className="md:text-5xl text-2xl font-bold md:leading-snug my-8">
+    <div className="my-10 flex flex-col">
+      <div className="flex-grow flex flex-col align-center text-center">
+        <h1 className="px-10 text-3xl md:text-5xl font-bold md:leading-snug my-8">
           Together,
           <br />
           we can create greater civic change
         </h1>
-        <p className="paragraph-1 landing-intro-paragraph mb-10">
+        <p className="w-4/5 md:w-1/2 text-xl md:text-2xl mb-10">
           CivicTechJobs unites ambitious technology practitioners with volunteer
           opportunities from a central hub of listings to build digital
           products, programs, and services.
         </p>
-        <Button
-          length="long"
-          size="lg"
-          addClass="landing-intro-btn"
-          href="/qualifier/1"
+        <Link
+          to="/qualifier/1"
+          className="h-min-12 px-14 py-3 mb-10 rounded-x-large text-xl font-bold bg-blue-dark hover:bg-blue-dark-hover hover:shadow-lg focus:bg-blue-dark-focused text-white"
         >
           Join us
-        </Button>
+        </Link>
+        <div
+          className="w-full h-fit bg-cover bg-no-repeat flex items-center justify-center -z-20"
+          style={{ backgroundImage: `url(${LandingPageBg})` }}
+        >
+          <img className="mt-[-10%] w-10/12 -z-10" src={LandingPageFg} alt="" />
+        </div>
       </div>
-      <div className="flex-column align-center text-center pb-10 landing-mission-container">
-        <img className="arrow-down mb-6" src={iconArrowDown} alt="" />
-        <h2 className="landing-mission-title text-4xl font-bold leading-normal mb-8">Our Mission</h2>
-        <p className="paragraph-2 landing-mission-paragraph">
+
+      <div className="flex-grow flex flex-col align-center  text-center bg-tan-light py-10">
+        <img
+          className="arrow-down mb-6 md:hidden"
+          src={iconArrowDown}
+          alt="Arrow Down Icon"
+        />
+        <h2 className="text-2xl md:text-4xl font-bold leading-normal mb-8">
+          Our Mission
+        </h2>
+        <p className="w-4/5 md:w-3/5 text-xl md:text-2xl">
           We bring together civic-minded volunteers from diverse backgrounds
           such as YOU to help local communities and governments. Thanks to the
           power of our volunteers, we are able to positively impact the
