0857689288962a226ad7e7c9aa1971c06c59aaca
Jimmy Juarez <jjuarez@boingo.com>
2024-06-14 13:53:29 -0700

    Create a new template using poetry and django

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for server project.
+URL configuration for api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
