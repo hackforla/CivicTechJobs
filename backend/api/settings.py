c9b049fee6794e10950a7716e107fa5b6d975d5a
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2024-06-28 09:40:32 -0700

    Add healthcheck api for new backend

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -111,6 +111,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+VERSION = '1.0.0'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
---
+++ backend/ctjbackend/uptime.py
@@ -0,0 +1,3 @@
+import datetime
+
+start_time = datetime.datetime.now()
