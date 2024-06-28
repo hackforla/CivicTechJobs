82be77817862978413bc36b88ce2a5ac8b6bf2ec
bennyv8 <bvanhr@gmail.com>
2024-06-27 18:25:13 -0700

    Pgdb connected
    - added configs in django.settings
    - installed psycopg2-binary **for development only**
    - psycopg2 for production requires certain system depdencies

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yourdatabase',
+        'USER': 'yourusername',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': 5432, #default port you don't need to mention in docker-compose
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
