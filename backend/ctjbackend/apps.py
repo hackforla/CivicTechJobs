d48741600b5075115ba2eb1f35941fa3373c584a
Bitian Zhang <86077274+bzzz-coding@users.noreply.github.com>
2024-07-01 09:53:24 -0700

    Add healthcheck api for new backend (#547)

--- backend/ctjbackend/apps.py
+++ backend/ctjbackend/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import ctjbackend.uptime
 
 
 class CtjbackendConfig(AppConfig):
--- backend/ctjbackend/views.py
+++ backend/ctjbackend/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+import datetime
+from ctjbackend.uptime import start_time
 
-# Create your views here.
+
+# Healthcheck view that returns the uptime of the app, a healthcheck message, and the version.
+def healthcheck(request):
+  current_time = datetime.datetime.now()
+  uptime_duration = current_time - start_time
+  uptime_seconds = int(uptime_duration.total_seconds())
+
+  hours = int(uptime_seconds // 3600)
+  minutes = int((uptime_seconds % 3600) // 60)
+  seconds = int(uptime_seconds % 60)
+
+  uptime_str = f"{hours:02d} hours {minutes:02d} minutes {seconds:02d} seconds"
+
+  context = {
+      "message": "healthcheck",
+      "uptime": uptime_str,
+      "version": settings.VERSION,
+  }
+  
+  return JsonResponse(context)
