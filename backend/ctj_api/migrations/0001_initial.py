e3933c1a2915e409e4d349096cc9a8ed45b74a81
Terence Lo <terencelomoa@gmail.com>
2024-09-19 22:47:41 -0700

    lint entire backend with Black

--- backend/ctj_api/migrations/0001_initial.py
+++ backend/ctj_api/migrations/0001_initial.py
@@ -9,26 +9,39 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Opportunities',
+            name="Opportunities",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('role', models.CharField(default='default-role', max_length=255)),
-                ('subrole', models.CharField(default='default-subrole', max_length=255)),
-                ('project', models.CharField(default='default-project', max_length=255)),
-                ('meetings_times', models.JSONField(default=list)),
-                ('difficulty_level', models.IntegerField(default=0)),
-                ('details', models.JSONField(default=dict)),
-                ('created_at', models.DateTimeField(default=django.utils.timezone.now)),
-                ('updated_at', models.DateTimeField(default=django.utils.timezone.now)),
-                ('updated_by', models.CharField(max_length=255)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("role", models.CharField(default="default-role", max_length=255)),
+                (
+                    "subrole",
+                    models.CharField(default="default-subrole", max_length=255),
+                ),
+                (
+                    "project",
+                    models.CharField(default="default-project", max_length=255),
+                ),
+                ("meetings_times", models.JSONField(default=list)),
+                ("difficulty_level", models.IntegerField(default=0)),
+                ("details", models.JSONField(default=dict)),
+                ("created_at", models.DateTimeField(default=django.utils.timezone.now)),
+                ("updated_at", models.DateTimeField(default=django.utils.timezone.now)),
+                ("updated_by", models.CharField(max_length=255)),
             ],
             options={
-                'db_table': 'opportunities',
+                "db_table": "opportunities",
             },
         ),
     ]
--- backend/ctj_api/serializers.py
+++ backend/ctj_api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from ctj_api.models import Opportunities
 
+
 class OpportunitiesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Opportunities
-        fields = ['id','role', 'subrole','project']
+        fields = ["id", "role", "subrole", "project"]
