e3ec8218a8d81adf7f9770d905167fc71db123ff
Terence Lo <terencelomoa@gmail.com>
2024-09-26 00:47:04 -0700

    Feature: lint python (#574)
    * install Black linter
    
    * lint entire backend with Black
    
    * install flake8 formatter
    
    * configure flake8
    
    * install isort
    
    * run isort to sort imports
    
    * fixed name pattern
    
    * added /api/healthcheck endpoint
    
    * install Black linter
    
    * lint entire backend with Black
    
    * install flake8 formatter
    
    * configure flake8
    
    * install isort
    
    * run isort to sort imports
    
    * install pre-commit
    
    * comment out admin and tests to pass flake8 linter rules
    
    * init pre-commit
    
    * remove trailing spaces
    
    * configure flake8 settings in pre-commit
    
    * delete obsolete poetry lock file
    
    * configure isort settings
    
    * remove precommit hook
    
    ---------
    
    Co-authored-by: Kevin <kevin31yu@gmail.com>
    Co-authored-by: Kevin Yu <118224034+kevin31yu@users.noreply.github.com>

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,28 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path, re_path, include
+
 from django.http import JsonResponse
+
+# from django.contrib import admin
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+
 
 # Custom handler for incorrect API routes
 def api_not_found(request, exception=None):
-    return JsonResponse({
-        'error': 'API endpoint not found',
-        'status_code': 404,
-        'message': 'The requested API endpoint does not exist'
-    }, status=404)
+    return JsonResponse(
+        {
+            "error": "API endpoint not found",
+            "status_code": 404,
+            "message": "The requested API endpoint does not exist",
+        },
+        status=404,
+    )
 
 
 urlpatterns = [
-    path('api/', include('ctj_api.urls'), name="api"),
-    # Custom error handler for invalid API routes
-    re_path(r'^api/.*$', api_not_found),  # Catch-all for incorrect API routes
-
+    path("api/", include("ctj_api.urls"), name="api"),
+    # Catch-all for incorrect API routes
+    re_path(r"^api/.*$", api_not_found),
     # Catch-all for frontend (React)
     re_path(
-        r'^.*$',
+        r"^.*$",
         TemplateView.as_view(template_name="index.html"),
         name="index",
     ),
