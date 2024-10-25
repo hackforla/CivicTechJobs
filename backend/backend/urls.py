9688865fa35165fe2f413fc829482ee4b61963bb
Terence Lo <terencelomoa@gmail.com>
2024-10-24 21:21:57 -0700

    run isort

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
-from django.urls import path, re_path, include
 from django.http import JsonResponse
+
+# from django.contrib import admin
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
 
