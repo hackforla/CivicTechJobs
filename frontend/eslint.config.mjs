bf444691608e66a3783257c5e27afb61d0b7b2e3
IraisValenzuela <iraisvq8@gmail.com>
2024-10-29 18:00:56 -0700

    added plugins for react hooks and jsx accessibility best practices

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,6 +4,8 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -32,6 +34,9 @@ export default [
       "tailwindcss/no-contradicting-classname": "error",
       "tailwindcss/no-unnecessary-arbitrary-value": "error",
       "tailwindcss/classnames-order": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "jsx-a11y/alt-text": "error",
     },
   },
   {
@@ -44,6 +49,8 @@ export default [
   {
     plugins: {
       tailwindcss: eslintPluginTailwindcss,
+      "react-hooks": eslintPluginReactHooks,
+      "jsx-a11y": eslintPluginJsxA11y,
     },
   },
 ];
