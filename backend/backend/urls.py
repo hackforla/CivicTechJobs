70a7e9ed99f7466275ba3930fbf9848125bfcc7b
Terence Lo <terencelomoa@gmail.com>
2024-11-09 14:41:37 -0800

    lint urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.http import JsonResponse
-
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
