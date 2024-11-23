f2e42b863542996a55df2fa06d59714b3972f65b
Terence Lo <terencelomoa@gmail.com>
2024-11-22 17:25:41 -0800

    add div

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -34,7 +34,7 @@ const router = createBrowserRouter([
           {
             path: "qualifier/:page",
             element: (
-              <Suspense fallback={"...loading"}>
+              <Suspense fallback={<div>...loading</div>}>
                 <QualifierPage />
               </Suspense>
             ),
