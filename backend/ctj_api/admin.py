525da2f62e47e7cd659851e9d5803a330e2624af
Bitian Zhang <86077274+bzzz-coding@users.noreply.github.com>
2024-12-03 17:59:24 -0800

    Added models and serializers for COP, roles, skills and project (#615)
    * Added models and serializers for COP, roles, skills and project
    
    * Updated Project model and registered newly added models in admin.py

--- backend/ctj_api/admin.py
+++ backend/ctj_api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Opportunities
+from .models import Opportunities, CommunityOfPractice, Role, Skill, Project
 
 # Register your models here.
 admin.site.register(Opportunities)
+admin.site.register(CommunityOfPractice)
+admin.site.register(Role)
+admin.site.register(Skill)
+admin.site.register(Project)
