9f7533c00ab0e24a5593171e438a587f36b359e6
Terence Lo <terencelomoa@gmail.com>
2025-03-10 16:40:49 -0700

    convert sass to TW

--- frontend/src/tw-components/FooterNav.tsx
+++ frontend/src/tw-components/FooterNav.tsx
@@ -36,24 +36,24 @@ const Logo = () => {
 
 function FooterNav() {
   return (
-    <footer className="footer-nav flex-container px-[176px]">
+    <footer className="box-border flex flex-col items-center gap-6 bg-blue-dark py-8 lg:flex-row lg:px-[176px]">
       <Logo />
       <nav
-        className="footer-menu flex-container"
+        className="flex max-lg:order-3 max-lg:w-[220px]"
         aria-label="footer-navigation"
       >
         {menuItems.map((item, index) => {
           return (
             <Fragment key={index}>
-              <div className="footer-menu-vertical-line"></div>
-              <Link className="footer-links" to={item.link}>
+              <div className="visible m-auto block w-5 rotate-90 border border-white first:hidden lg:invisible lg:w-p5 lg:first:visible lg:first:block"></div>
+              <Link className="text-[16px] font-bold text-white" to={item.link}>
                 {item.name}
               </Link>
             </Fragment>
           );
         })}
       </nav>
-      <div className="footer-donate-button flex-container">
+      <div className="mx-0 flex lg:ml-auto">
         <Button
           color="primary-dark"
           href="https://www.hackforla.org/donate/"
