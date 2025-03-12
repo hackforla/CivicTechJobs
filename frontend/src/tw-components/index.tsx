d7a0a34f0225f869fda667b27f895c9e8784d7c4
Terence Lo <terencelomoa@gmail.com>
2025-03-12 16:48:14 -0700

    Feature: Design system, FooterNav (#653)
    * clean up tailwind config
    
    * fix border radius
    
    * refactor Logo
    
    * init tw footerNav
    
    * convert sass to TW
    
    * replace old FooterNavs
    
    * delete old FooterNav

--- frontend/src/tw-components/index.tsx
+++ frontend/src/tw-components/index.tsx
@@ -1,4 +1,5 @@
 export { default as AuthNav } from "./AuthNav";
-export { default as HeaderNav } from "./HeaderNav";
 export { default as CookieBanner } from "./CookieBanner";
+export { default as FooterNav } from "./FooterNav";
+export { default as HeaderNav } from "./HeaderNav";
 export { default as TextField } from "./TextField";
